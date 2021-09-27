6c56d9aa3ab7f117196aacc9bfca61cb4361057a
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-27 10:30:26 +0200

    Added the weekend exercise

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -1,25 +1,22 @@
-let kontejner = document.querySelector('.kontejner');
-let colOne = document.querySelector('.One');
-let colTwo = document.querySelector('.Two');
-let colThree = document.querySelector('.Three');
-let colFour = document.querySelector('.Four');
+const openModal = document.getElementsByClassName('openModal');
+const modal = document.getElementsByClassName('modal');
+const close = document.getElementsByClassName('close');
 
-for(let i = 1; i <= 10; i++) {
-    let divImg = document.createElement('div');
-    let divImage = document.createElement('img');
-    divImage.setAttribute('src', `../images/img${i}.jpg`);
-    divImg.appendChild(divImage);
-
-    if(i === 1 || i === 5 || i === 8) {
-        colOne.appendChild(divImg);
-    }
-    else if(i === 2 || i === 6) {
-        colTwo.appendChild(divImg);
-    }
-    else if(i === 3 || i === 7 || i === 9) {
-        colThree.appendChild(divImg);
-    }
-    else {
-        colFour.appendChild(divImg);
+function DodajEventListener_v1() {
+    for(let i=0; i < openModal.length; i++) {
+        openModal[i].addEventListener('click',  () => {
+        modal[i].classList.add('on');
+        })
     }
 }
+
+function DodajEventListener_v2() {
+    for (let i=0; i < close.length; i++) {
+        close[i].addEventListener('click',  () => {
+            modal[i].classList.remove('on');
+        })
+    }
+}
+
+DodajEventListener_v1();
+DodajEventListener_v2();
